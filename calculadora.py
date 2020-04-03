71317714b26766e949a65a90bcbb9a6d7c858cad
mitsiu <mitsiu.carreno@gmail.com>
2020-04-02 18:32:26 -0600

    menu de calculadora

--- calculadora.py
+++ calculadora.py
@@ -25,7 +25,31 @@
 
 
 def main():
-    print("Hola");
+    operation = None;
+    num1 = None;
+    num2 = None;
+
+    Print(Operation:);
+    Print(1 +)
+    Print(2 -)
+    Print(3 *)
+    Print(4 /)
+    operation = int(raw_input("Option:"))
+
+    num1 = float(raw_input("First number"))
+    num2 = float(raw_input("Second number"))
+
+
+    if operation == 1 :
+        suma(num1, num2);
+    elif operation == 2 :
+        resta(num1, num2);
+    elif operation == 3 :
+        multip(num1, num2);
+    elif operation == 4 :
+        division(num1, num2);
+    else
+        print("bye")
 
 if __name__ == '__main__':
     main();
